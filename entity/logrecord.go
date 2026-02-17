58cb0097f4633e40c1a43950ddf44f28098eb3ad
thisisjab <heythisisjab@gmail.com>
2026-02-17 12:37:27 +0330

    refactor(entity): remove `IsProcessed` field and omit `RawData`

--- entity/logrecord.go
+++ entity/logrecord.go
@@ -21,14 +21,13 @@ func (l LogLevel) String() string {
 	return [...]string{"UNKNOWN", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"}[l]
 }
 
-// LogRecord represents a log record that is not processed and received from a log source.
+// LogRecord represents a log record received from a log source.
 type LogRecord struct {
-	ID          uuid.UUID      `json:"id"`
-	IsProcessed bool           `json:"is_processed"`
-	Source      string         `json:"source"`
-	RawData     []byte         `json:"raw_data"`
-	Level       LogLevel       `json:"level"`
-	Timestamp   time.Time      `json:"timestamp"`
-	Message     string         `json:"message"`
-	Metadata    map[string]any `json:"metadata"`
+	ID        uuid.UUID      `json:"id"`
+	Source    string         `json:"source"`
+	RawData   []byte         `json:"-"`
+	Level     LogLevel       `json:"level"`
+	Timestamp time.Time      `json:"timestamp"`
+	Message   string         `json:"message"`
+	Metadata  map[string]any `json:"metadata"`
 }
