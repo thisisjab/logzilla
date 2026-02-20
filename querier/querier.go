cff776cba350aea7e1a49adc34ca038def2591a8
thisisjab <heythisisjab@gmail.com>
2026-02-20 14:49:02 +0330

    refactor(querier): simplify query request interface by moving fields to query

--- querier/querier.go
+++ querier/querier.go
@@ -2,11 +2,8 @@ package querier
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/thisisjab/logzilla/entity"
-	"github.com/thisisjab/logzilla/fault"
 )
 
 type QueryDirection string
@@ -18,41 +15,6 @@ const (
 
 type QueryRequest struct {
 	Query Query
-	// Below fields are used for time-based pagination.
-	Start  time.Time
-	End    time.Time
-	Limit  int
-	Cursor string
-}
-
-func (r QueryRequest) Validate() error {
-	// MAYBE: In future we may want to read these from configs.
-	const LimitMin = 1
-	const LimitMax = 1000
-
-	if r.Limit > LimitMax {
-		return fault.New(fault.BadInputCode, "").WithMetadata(fault.FieldErrorsMetadata{"limit": []string{fmt.Sprintf("Values larger than %d are not supported.", LimitMax)}})
-	}
-
-	if r.Limit < LimitMin {
-		return fault.New(fault.BadInputCode, "").WithMetadata(fault.FieldErrorsMetadata{"limit": []string{fmt.Sprintf("Values smaller than %d are not supported.", LimitMin)}})
-	}
-
-	if r.Start.IsZero() {
-		return fault.New(fault.BadInputCode, "").WithMetadata(fault.FieldErrorsMetadata{"start": []string{"Field is required."}})
-	}
-
-	return nil
-}
-
-// GetQueryDirection helps storage implementations to determine if query must be in an ascending order or descending.
-func (r QueryRequest) GetQueryDirection() QueryDirection {
-	// If End is before Start, user wants to go backwards in time
-	if !r.End.IsZero() && r.End.Before(r.Start) {
-		return QueryDirectionBackward
-	}
-	// Default to Forward
-	return QueryDirectionForward
 }
 
 type QueryResponse struct {
--- querier/query.go
+++ querier/query.go
@@ -1,8 +1,25 @@
 package querier
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/thisisjab/logzilla/fault"
+)
+
+// Expr represents a boolean expression (evaluates to true/false)
+type Expr interface {
+	exprNode()
+}
+
 type Query struct {
 	Expr Expr
 	Sort []SortField
+	// Below fields are used for time-based pagination.
+	Start  time.Time
+	End    time.Time
+	Limit  int
+	Cursor string
 }
 
 type SortField struct {
@@ -10,7 +27,32 @@ type SortField struct {
 	IsDescending bool
 }
 
-// Expr represents a boolean expression (evaluates to true/false)
-type Expr interface {
-	exprNode()
+func (r Query) Validate() error {
+	// MAYBE: In future we may want to read these from configs.
+	const LimitMin = 1
+	const LimitMax = 1000
+
+	if r.Limit > LimitMax {
+		return fault.New(fault.BadInputCode, "").WithMetadata(fault.FieldErrorsMetadata{"limit": []string{fmt.Sprintf("Values larger than %d are not supported.", LimitMax)}})
+	}
+
+	if r.Limit < LimitMin {
+		return fault.New(fault.BadInputCode, "").WithMetadata(fault.FieldErrorsMetadata{"limit": []string{fmt.Sprintf("Values smaller than %d are not supported.", LimitMin)}})
+	}
+
+	if r.Start.IsZero() {
+		return fault.New(fault.BadInputCode, "").WithMetadata(fault.FieldErrorsMetadata{"start": []string{"Field is required."}})
+	}
+
+	return nil
+}
+
+// GetQueryDirection helps storage implementations to determine if query must be in an ascending order or descending.
+func (r Query) GetQueryDirection() QueryDirection {
+	// If End is before Start, user wants to go backwards in time
+	if !r.End.IsZero() && r.End.Before(r.Start) {
+		return QueryDirectionBackward
+	}
+	// Default to Forward
+	return QueryDirectionForward
 }
