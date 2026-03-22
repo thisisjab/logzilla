828cd353e22982665d551d165e5cd2f2d755c5db
thisisjab <heythisisjab@gmail.com>
2026-03-23 01:45:31 +0330

    feat(ui): add basic error handling

--- ui/src/components/Logs/QuerySection.tsx
+++ ui/src/components/Logs/QuerySection.tsx
@@ -3,20 +3,39 @@ import QueryInput from "./QueryInput"
 import QueryResult from "./QueryResult"
 import { useSearchLogs } from "../../hooks/useSearchLogs";
 import LoadingIndicator from "../Shared/LoadingIndicator";
+import { AxiosError } from "axios";
 
 function QuerySection() {
     const [query, setQuery] = useState<string>("");
-    const { data, isLoading } = useSearchLogs(query);
+    const { data, isLoading, error, isError } = useSearchLogs({query: query});
 
     // TODO: add error handling
 
     return (
         <main className="max-w-full text-nowrap m-5 grow flex flex-col overflow-hidden gap-3">
+            {/* Query input for writing queriers */}
             <QueryInput onQuerySubmit={q => setQuery(q)} />
+
+            {/* Dirty error handling
+
+            DO NOT PANIC!
+
+            This is really intentional since I was not in mood of adding react-hook-form, and other boilerplates.
+            Plus, it works just fine.
+            If we introduced other forms later, we can switch to react-hook-form + zod
+            */}
+            {isError && error instanceof AxiosError && error.status === 422 && <div>Query error: {error.response?.data?.metadata.fields.query}</div>}
+            {isError && error instanceof AxiosError && error.status === 400 && <div>Query error: {error.response?.data?.message}</div>}
+            {isError && !(error instanceof AxiosError ) &&  <div>Unexpected error.</div>}
+
+
+            {/* Loading indicator */}
             {isLoading &&
                 <div className="flex justify-center items-center grow ">
                     <LoadingIndicator />
                 </div>}
+
+            {/* Table for showing result */}
             {data?.data.data !== undefined && <QueryResult logs={data?.data.data} />}
         </main >
     )
--- ui/src/hooks/useSearchLogs.ts
+++ ui/src/hooks/useSearchLogs.ts
@@ -2,18 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import { apiClient } from "../api/apiClient";
 import { type LogRecord } from "../api/models/LogRecord";
 import type { PaginatedAPIResponse } from "../api/models/Response";
+import type { SearchLogRequest } from "../api/schemas/SearchLog";
 
-const searchLogs = (query: string) => {
+const searchLogs = (req: SearchLogRequest) => {
   return apiClient
     .post<PaginatedAPIResponse<LogRecord>>("/logs/search", {
-      query: query,
+      query: req.query,
     })
     .then((res) => res);
 };
 
-export const useSearchLogs = (query: string) => {
+export const useSearchLogs = (req: SearchLogRequest) => {
   return useQuery({
-    queryKey: ["logs", query],
-    queryFn: () => searchLogs(query),
+    queryKey: ["logs", req.query],
+    queryFn: () => searchLogs(req),
+    retry: false,
   });
 };
